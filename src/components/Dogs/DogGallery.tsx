5a796e36bd355725980fcf503fae3808e1934a89
Shyam <gshyamprasad2001@gmail.com>
2025-03-19 22:41:11 +0530

    aaded infi scroll

--- src/components/Dogs/DogGallery.tsx
+++ src/components/Dogs/DogGallery.tsx
@@ -13,12 +13,41 @@ const fetchDogs = async ({ pageParam = 1 }): Promise<DogProps> => {
   return res.json();
 };
 
-export default function DogGallery(){
+export default function DogGallery() {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery<DogProps>({
+      queryKey: ["dogs"],
+      queryFn:  fetchDogs,
+      getNextPageParam: (lastPage, allPages) => allPages+1
+       
+    });
 
+  const { ref } = useInView({
+    threshold: 1.0,
+    onChange: (inView) => inView && hasNextPage && fetchNextPage(),
+    triggerOnce: false,
+  });
 
-  
   return (
-    <>
-    </>
-  )
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center py-10">
+      <h1 className="text-xl text-center m-8 p-6">Infinite Scroll</h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 w-max-6xl">
+        {data?.pages.map((page, pageIndex) =>
+          page.message.map((url, index) => (
+            <img
+              key={`${pageIndex}-${index}`}
+              src={url}
+              alt="dog"
+              className="h-40 w-full object-cover hover:scale-105 shadow rounded-md"
+            />
+          ))
+        )}
+      </div>
+      <div ref={ref} className="mt-5 text-lg font-semibold text-gray-600">
+        {isFetchingNextPage
+          ? "Loading more dogs... 🐾"
+          : "Scroll down for more!"}
+      </div>
+    </div>
+  );
 }
